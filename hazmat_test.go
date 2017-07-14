26147f95901cc5c5894814466710aca0652043ff
Daan Sprenkels <hello@dsprenkels.com>
2017-07-14 11:44:59 +0800

    Run go fmt

--- hazmat_test.go
+++ hazmat_test.go
@@ -1,74 +1,71 @@
 package sss
 
 import (
-    "bytes"
-    "errors"
-    "math/rand"
-    "testing"
-    "testing/quick"
+	"bytes"
+	"errors"
+	"math/rand"
+	"testing"
+	"testing/quick"
 )
 
-
 func makeKey(c byte) []byte {
-    key := make([]byte, 32)
-    for i, _ := range key {
-        key[i] = c
-    }
-    return key
+	key := make([]byte, 32)
+	for i, _ := range key {
+		key[i] = c
+	}
+	return key
 }
-
 
 func TestCreateKeyshares(t *testing.T) {
-    key := makeKey(42)
-    keyshares, err := CreateKeyshares(key, 5, 4)
-    if err != nil {
-        t.Error(err)
-    }
-    if len(keyshares) != 5 {
-        t.Error(errors.New("expected 5 keyshares"))
-    }
-    for _, keyshare := range keyshares {
-        if len(keyshare) != 33 {
-            t.Error(errors.New("keyshare should be 33 bytes long"))
-        }
-    }
+	key := makeKey(42)
+	keyshares, err := CreateKeyshares(key, 5, 4)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(keyshares) != 5 {
+		t.Error(errors.New("expected 5 keyshares"))
+	}
+	for _, keyshare := range keyshares {
+		if len(keyshare) != 33 {
+			t.Error(errors.New("keyshare should be 33 bytes long"))
+		}
+	}
 }
 
-
 func TestCombineKeyshares(t *testing.T) {
-    f := func(n, k, k2 int, keyArr [32]byte) bool {
-        n, k, k2 = n & 0xff, k & 0xff, k2 & 0xff
-        key := keyArr[:]
+	f := func(n, k, k2 int, keyArr [32]byte) bool {
+		n, k, k2 = n&0xff, k&0xff, k2&0xff
+		key := keyArr[:]
 
-        // Unable to test if not n is not larger than k and k2
-        if k > n || k2 > n || k < 1 {
-            return true
-        }
+		// Unable to test if not n is not larger than k and k2
+		if k > n || k2 > n || k < 1 {
+			return true
+		}
 
-        keyshares, err := CreateKeyshares(key, n, k)
-        if err != nil {
-            t.Error(err)
-        }
+		keyshares, err := CreateKeyshares(key, n, k)
+		if err != nil {
+			t.Error(err)
+		}
 
-        // Throw some of the keyshares away
-        new_keyshares := make([][]byte, k2);
-        for i, idx := range rand.Perm(n)[:k2] {
-            new_keyshares[i] = keyshares[idx]
-        }
+		// Throw some of the keyshares away
+		new_keyshares := make([][]byte, k2)
+		for i, idx := range rand.Perm(n)[:k2] {
+			new_keyshares[i] = keyshares[idx]
+		}
 
-        // Combine the filtered keyshares
-        restored, err := CombineKeyshares(new_keyshares)
-        if err != nil {
-            if k2 == 0 {
-                return true
-            }
-            t.Error(err)
-        }
+		// Combine the filtered keyshares
+		restored, err := CombineKeyshares(new_keyshares)
+		if err != nil {
+			if k2 == 0 {
+				return true
+			}
+			t.Error(err)
+		}
 
-        return (k <= k2) == bytes.Equal(key, restored)
-    }
+		return (k <= k2) == bytes.Equal(key, restored)
+	}
 
-    if err := quick.Check(f, &quick.Config{MaxCountScale: 10}); err != nil {
-        t.Error(err)
-    }
+	if err := quick.Check(f, &quick.Config{MaxCountScale: 10}); err != nil {
+		t.Error(err)
+	}
 }
--- sss_test.go
+++ sss_test.go
@@ -1,116 +1,110 @@
 package sss
 
 import (
-    "bytes"
-    "math/rand"
-    "testing"
-    "testing/quick"
+	"bytes"
+	"math/rand"
+	"testing"
+	"testing/quick"
 )
 
-
 func makeData(c byte) []byte {
-    data := make([]byte, 64)
-    for i, _ := range data {
-        data[i] = c
-    }
-    return data
+	data := make([]byte, 64)
+	for i, _ := range data {
+		data[i] = c
+	}
+	return data
 }
-
 
 func TestCreateShares(t *testing.T) {
-    data := makeData(42)
-    shares, err := CreateShares(data, 5, 4)
-    if err != nil {
-        t.Fail()
-    }
-    if len(shares) != 5 {
-        t.Fail()
-    }
-    for _, share := range shares {
-        if len(share) != 113 {
-            t.Fail()
-        }
-    }
+	data := makeData(42)
+	shares, err := CreateShares(data, 5, 4)
+	if err != nil {
+		t.Fail()
+	}
+	if len(shares) != 5 {
+		t.Fail()
+	}
+	for _, share := range shares {
+		if len(share) != 113 {
+			t.Fail()
+		}
+	}
 }
-
 
 func TestCombineShares(t *testing.T) {
-    f := func(n, k, k2 int, dataArr [64]byte) bool {
-        n, k, k2 = n & 0xff, k & 0xff, k2 & 0xff
-        data := dataArr[:]
+	f := func(n, k, k2 int, dataArr [64]byte) bool {
+		n, k, k2 = n&0xff, k&0xff, k2&0xff
+		data := dataArr[:]
 
-        // We can't really test anything if not n is not larger than k and k2
-        if k > n || k2 > n || k < 1 {
-            return true
-        }
+		// We can't really test anything if not n is not larger than k and k2
+		if k > n || k2 > n || k < 1 {
+			return true
+		}
 
-        shares, err := CreateShares(data, n, k)
-        if err != nil {
-            t.Error(err)
-        }
+		shares, err := CreateShares(data, n, k)
+		if err != nil {
+			t.Error(err)
+		}
 
-        // Throw some of the shares away
-        new_shares := make([][]byte, k2);
-        for i, idx := range rand.Perm(n)[:k2] {
-            new_shares[i] = shares[idx]
-        }
+		// Throw some of the shares away
+		new_shares := make([][]byte, k2)
+		for i, idx := range rand.Perm(n)[:k2] {
+			new_shares[i] = shares[idx]
+		}
 
-        // Combine the filtered shares
-        restored, err := CombineShares(new_shares)
-        if err != nil {
-            if k2 == 0 {
-                return true
-            }
-            t.Error(err)
-        }
+		// Combine the filtered shares
+		restored, err := CombineShares(new_shares)
+		if err != nil {
+			if k2 == 0 {
+				return true
+			}
+			t.Error(err)
+		}
 
-        if k2 < k {
-            return restored == nil
-        } else {
-            return bytes.Equal(data, restored)
-        }
-    }
+		if k2 < k {
+			return restored == nil
+		} else {
+			return bytes.Equal(data, restored)
+		}
+	}
 
-    if err := quick.Check(f, &quick.Config{MaxCountScale: 10}); err != nil {
-        t.Error(err)
-    }
+	if err := quick.Check(f, &quick.Config{MaxCountScale: 10}); err != nil {
+		t.Error(err)
+	}
 }
-
 
 func TestDuplicateShares(t *testing.T) {
-    data := makeData(42)
-    shares, err := CreateShares(data, 3, 2)
+	data := makeData(42)
+	shares, err := CreateShares(data, 3, 2)
 
-    // Remove one and maybe add duplicates
-    new_shares := make([][]byte, 0, 3);
-    new_shares = append(new_shares, shares[1])
-    new_shares = append(new_shares, shares[0])
-    new_shares = append(new_shares, shares[1])
+	// Remove one and maybe add duplicates
+	new_shares := make([][]byte, 0, 3)
+	new_shares = append(new_shares, shares[1])
+	new_shares = append(new_shares, shares[0])
+	new_shares = append(new_shares, shares[1])
 
-    // Combine the new set of shares
-    restored, err := CombineShares(new_shares)
-    if err != nil || restored == nil {
-        t.Error(err)
-    }
+	// Combine the new set of shares
+	restored, err := CombineShares(new_shares)
+	if err != nil || restored == nil {
+		t.Error(err)
+	}
 }
-
 
 func BenchmarkCreateShares(b *testing.B) {
-    data := makeData(42)
-    for i := 0; i < b.N; i++ {
-        CreateShares(data, 5, 4)
-    }
+	data := makeData(42)
+	for i := 0; i < b.N; i++ {
+		CreateShares(data, 5, 4)
+	}
 }
 
-
 func BenchmarkCombineShares(b *testing.B) {
-    data := makeData(42)
-    shares, err := CreateShares(data, 5, 4)
-    if err != nil {
-        b.Error()
-    }
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        CombineShares(shares)
-    }
+	data := makeData(42)
+	shares, err := CreateShares(data, 5, 4)
+	if err != nil {
+		b.Error()
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CombineShares(shares)
+	}
 }
